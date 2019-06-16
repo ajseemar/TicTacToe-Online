fee9e32ef69cc6735cc9b3c0938eff68c6573ef9
aseemar <ajseemar@gmail.com>
2019-06-16 11:38:20 -0700

    add webpack script to package.json

--- src/grid.js
+++ src/grid.js
@@ -1,6 +1,12 @@
 class Grid {
     constructor () {
-        this.grid = new Array(3).map(el => new Array(3));
+        this.grid = this.generateGrid();
+    }
+
+    generateGrid () {
+        const grid = new Array(3);
+        for (let i = 0; i < grid.length; i++) grid[i] = new Array(3);
+        return grid;
     }
 }
 
