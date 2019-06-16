a0458fa97542407278ef25ca26efb1f4c4643b18
David Agustin <davidsyagustin@gmail.com>
2019-06-16 11:42:00 -0700

    Serve Static Files on Server

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app = express();
 const serv = require('http').Server(app);
 const io = require('socket.io')(serv, {});
 const port = process.env.PORT || 3000;
-serv.listen(port, function () {
-    console.log('Listening to port: ' + port);
-});
-
 const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './public')))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+serv.listen(port, function () {
+    console.log('Listening to port: ' + port);
+});
