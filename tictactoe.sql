032293cf9592c8cf9e316c59186947194cfce7d4
David Agustin <davidsyagustin@gmail.com>
2019-06-16 12:05:11 -0700

    Create sql File

---
+++ tictactoe.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS tictactoe;
+
+CREATE DATABASE tictactoe;
+
+CREATE SCHEMA IF NOT EXISTS user_data;
+CREATE SCHEMA IF NOT EXISTS game_data;
+
+DROP TABLE tictactoe.user_data;
+DROP TABLE tictactoe.game_data;
+
+CREATE TABLE tictactoe.user_data
+(
+  id                    SERIAL PRIMARY KEY,
+  test                  INT
+);
+
+CREATE TABLE tictactoe.game_data
+(
+  id                    SERIAL PRIMARY KEY,
+  test                  INT
+);
